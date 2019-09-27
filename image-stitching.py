f19fd87c102951b5ed7a7589a4132e0c1aafdc88
Erik Nilsson <eriknilsssonn@gmail.com>
2019-09-27 11:37:36 +0200

    Added code for detecting keypoints

--- image-stitching.py
+++ image-stitching.py
@@ -7,3 +7,9 @@ img_ = cv2.imread(‘right.JPG’)
 img1 = cv2.cvtColor(img_,cv2.COLOR_BGR2GRAY)
 img = cv2.imread(‘left.JPG’)
 img2 = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+
+sift = cv2.xfeatures2d.SIFT_create()
+# Find keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1, None)
+kp2, des2 = sift

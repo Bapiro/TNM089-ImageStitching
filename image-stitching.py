a8187589d0b5aa638ee25b3755749a985b8d3e1a
Bapiro <lucas.rung@gmail.com>
2019-10-07 14:48:43 +0200

    took away old code from color corrections

--- image-stitching.py
+++ image-stitching.py
@@ -42,18 +42,6 @@ else:
     raise AssertionError("Cant' find enough keypoints")
 
 
-img_yuv = cv2.cvtColor(img_, cv2.COLOR_BGR2YUV)
-img_yuv2 = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
-
-# equalize the histogram of the Y channel
-img_yuv[:, :, 0] = cv2.equalizeHist(img_yuv[:, :, 0])
-
-img_yuv2[:, :, 0] = cv2.equalizeHist(img_yuv2[:, :, 0])
-
-# convert the YUV image back to RGB format
-img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
-img_output2 = cv2.cvtColor(img_yuv2, cv2.COLOR_YUV2BGR)
-
 img_output = image_hist.hist_match(img_, img)
 
 img_output2 = img

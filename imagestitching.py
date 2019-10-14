89c043c0b3052efef3124dd6b40c8857a27b9e2b
Erik Nilsson <eriknilsssonn@gmail.com>
2019-10-14 09:44:22 +0200

    ADdded functionality for multiple images

--- imagestitching.py
+++ imagestitching.py
@@ -7,83 +7,81 @@ from sklearn.cluster import KMeans
 import image_hist
 import random
 
-# Convert images to greyscale
-img_ = cv2.imread('./Images/right.JPG')
-img = cv2.imread('./Images/left.JPG')
+def stitching(img_, img):
+
+    # Convert images to greyscale
+    #img_ = cv2.imread('./Images/right.JPG')
+    #img = cv2.imread('./Images/left.JPG')
 
 
-img1 = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
-img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img1 = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-sift = cv2.xfeatures2d.SIFT_create()
-# Find keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1, None)
-kp2, des2 = sift.detectAndCompute(img2, None)
+    sift = cv2.xfeatures2d.SIFT_create()
+    # Find keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(img1, None)
+    kp2, des2 = sift.detectAndCompute(img2, None)
 
-bf = cv2.BFMatcher()
-# Collect the two best matches in des1 and des2
-matches = bf.knnMatch(des1, des2, k=2)
+    bf = cv2.BFMatcher()
+    # Collect the two best matches in des1 and des2
+    matches = bf.knnMatch(des1, des2, k=2)
 
-# Apply ratio test
-good = []
-for m in matches:
-    if m[0].distance < 0.5*m[1].distance:
-        good.append(m)
-        matches = np.asarray(good)
-
-
-# Image alignment
-if len(matches[:, 0]) >= 4:
-    src = np.float32(
-        [kp1[m.queryIdx].pt for m in matches[:, 0]]).reshape(-1, 1, 2)
-    dst = np.float32(
-        [kp2[m.trainIdx].pt for m in matches[:, 0]]).reshape(-1, 1, 2)
-    H, masked = cv2.findHomography(src, dst, cv2.RANSAC, 5.0)
-    # print(H)
-else:
-    raise AssertionError("Cant' find enough keypoints")
+    # Apply ratio test
+    good = []
+    for m in matches:
+        if m[0].distance < 0.5*m[1].distance:
+            good.append(m)
+            matches = np.asarray(good)
 
 
-img_output = np.uint8(image_hist.hist_match(img_, img))
+    # Image alignment
+    if len(matches[:, 0]) >= 4:
+        src = np.float32(
+            [kp1[m.queryIdx].pt for m in matches[:, 0]]).reshape(-1, 1, 2)
+        dst = np.float32(
+            [kp2[m.trainIdx].pt for m in matches[:, 0]]).reshape(-1, 1, 2)
+        H, masked = cv2.findHomography(src, dst, cv2.RANSAC, 5.0)
+        # print(H)
+    else:
+        raise AssertionError("Cant' find enough keypoints")
 
 
-img_output2 = img
+    #img_ = np.uint8(image_hist.hist_match(img_, img))
 
-print(img_output2)
-
-
-# Stitching
-dst = cv2.warpPerspective(
-    img_output, H, (img.shape[1] + img_.shape[1], img.shape[0]))
-plt.subplot(122), plt.imshow(dst), plt.title('Warped Image')
-plt.show()
-plt.figure()
-#dst[0:img.shape[0], 0:img.shape[1]] = img_output2
+    # Stitching
+    dst = cv2.warpPerspective(
+        img_, H, (img.shape[1] + img_.shape[1], img.shape[0]+int((img_.shape[0])/2)))
+    #plt.subplot(122), plt.imshow(dst), plt.title('Warped Image')
+    #plt.show()
+    #plt.figure()
+    #dst[0:img.shape[0], 0:img.shape[1]] = img
 
 
-# Taking random pixel from left/right image in a 20px width
-startLength = img.shape[1]-20
-dst[0:img.shape[0], 0:startLength] = img_output2[0:img.shape[0], 0:startLength]
-rnd = 0
-for x in range(startLength, img.shape[1]):
-    for y in range(img.shape[0]):
-        rnd = random.randrange(0, 2)
-        if(rnd == 1):
-            dst[y, x] = img_output2[y, x]
-            #print(1)
-        #else:
-            #print(0)
+    # Taking random pixel from left/right image in a 20px width
+    startLength = img.shape[1]-20
+    dst[0:img.shape[0], 0:startLength] = img[0:img.shape[0], 0:startLength]
+    rnd = 0
+    for x in range(startLength, img.shape[1]):
+        for y in range(img.shape[0]):
+            rnd = random.randrange(0, 2)
+            if(rnd == 1):
+                dst[y, x] = img[y, x]
+                #print(1)
+            #else:
+                #print(0)
 
-print(img.shape[0])
-# blurred_img = cv2.medianBlur(dst, 3)
-# blurred_img = cv2.GaussianBlur(dst, (21, 1), 0)
-# blurred_img = cv2.blur(img, (5, 5))
-# mask = np.zeros(dst.shape, "uint8")
-# mask = cv2.rectangle(mask, (img.shape[1]-30, 0), (img.shape[1]+30, img.shape[0]), (255, 255, 255), -1)
+    # print(img.shape[0])
+    # blurred_img = cv2.medianBlur(dst, 3)
+    # blurred_img = cv2.GaussianBlur(dst, (21, 1), 0)
+    # blurred_img = cv2.blur(img, (5, 5))
+    # mask = np.zeros(dst.shape, "uint8")
+    # mask = cv2.rectangle(mask, (img.shape[1]-30, 0), (img.shape[1]+30, img.shape[0]), (255, 255, 255), -1)
 
 
-# out = np.where(mask == (0, 0, 0), dst, blurred_img)
+    # out = np.where(mask == (0, 0, 0), dst, blurred_img)
 
-cv2.imwrite('output.jpg', dst)
-plt.imshow(dst)
-plt.show()
+    cv2.imwrite('output.jpg', dst)
+    #plt.imshow(dst)
+    #plt.show()
+
+    return dst
--- main.py
+++ main.py
@@ -1,14 +1,59 @@
 #import imagestitching.py
 import cv2
 import numpy as np
-import laplacianpyramid.py as lp
+import matplotlib.pyplot as plt
+import imagestitching
+import image_hist
 
 images = [
-    cv2.cvtColor(cv2.imread('./Images/left.jpg'), cv2.COLOR_BGR2GRAY), 
-    cv2.cvtColor(cv2.imread('./Images/right.jpg'), cv2.COLOR_BGR2GRAY)
+    #cv2.cvtColor(cv2.imread('./Images/right.jpg'), cv2.COLOR_BGR2GRAY), 
+    #cv2.cvtColor(cv2.imread('./Images/left.jpg'), cv2.COLOR_BGR2GRAY)
+    cv2.imread('./Images/multipleTest1.jpg'),
+    cv2.imread('./Images/multipleTest2.jpg'),
+    cv2.imread('./Images/multipleTest3.jpg'),
+    cv2.imread('./Images/multipleTest4.jpg')
 ]
 
+#finalimg = imagestitching.stitching(img, img)
+finalimg = images[0]
+
+imgList = []
+
+imgList.append(finalimg)
+
+# Color correction
+counter = 0
+for img in images[1:]:
+    counter = counter + 1
+    images[counter] = np.uint8(image_hist.hist_match(img, images[0]))
 
 
-for img in images:
-    print("hej")
+
+# Stitch the images together
+for img in images[1:]:
+    finalimg = imagestitching.stitching(finalimg, img)
+    imgList.append(finalimg)
+
+#print(len(imgList))
+
+
+plt.subplot(122), plt.imshow(imgList[0]), plt.title('img 1')
+plt.show()
+plt.figure()
+
+plt.subplot(122), plt.imshow(imgList[1]), plt.title('img 2')
+plt.show()
+plt.figure()
+
+plt.subplot(122), plt.imshow(imgList[2]), plt.title('img 3')
+plt.show()
+plt.figure()
+
+plt.subplot(122), plt.imshow(imgList[3]), plt.title('Warped Image')
+plt.show()
+plt.figure()
+
+
+cv2.imwrite('test.jpg', finalimg) 
+
+    
